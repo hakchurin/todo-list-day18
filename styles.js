2c19af7000cf19117023123efd665757485184e2
Haley Akchurin <hdakchurin@gmail.com>
2016-07-27 09:12:29 -0500

    todo list

--- styles.js
+++ styles.js
@@ -1,25 +1,60 @@
-$('input[type="submit"]').on('click', function(evt) {
+$('input[type="submit" ]').on('click', function(evt) {
     evt.preventDefault();
-
 
     var task = document.querySelector('input[type="text"]').value;
 
     var list = $('.lists');
-    var button = $('<button>complete</button>');
+    var button = $('<button>X</button>');
     var item = $('<li></li>');
+
+
     item.append(task);
     list.append(item);
     list.append(button);
 
-    $(button).on('click', function() {
-        item.remove();
-        button.remove();
+    // $(button).on('click', function() {
+    //     item.remove();
+    //     button.remove();
 
+    // });
+
+
+    // var form = document.getElementById("myForm");
+    // form.reset();
+
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://tiny-za-server.herokuapp.com/collections/hakchurintodo',
+        contentType: 'application/JSON',
+        data: JSON.stringify({
+            todo: task
+        }),
+        success: function(postResponse) {
+          console.log('hi');
+            button.on('click', function() {
+            item.remove();
+            button.remove();
+
+            $.ajax({
+                type: 'DELETE',
+                url: 'http://tiny-za-server.herokuapp.com/collections/hakchurintodo/' + postResponse._id,
+                success: function(deleteResponse) {
+                    console.log('it deleted');
+
+                },
+                error: function() {
+
+                    console.log('it didnt delete');
+                }
+
+
+            });
+
+
+        });
+      }
     });
-
-
-    var form = document.getElementById("myForm");
-    form.reset();
 
 });
 
@@ -30,96 +65,43 @@ $('input[type="submit"]').on('click', function(evt) {
 //
 
 
-var submits = {
 
-    "name": "Joey",
-    "color": "white",
-    "powers": ["healing", "healing"],
-    "last": false
-};
 
-var postedItems =
-    $.ajax({
-        url: 'http://tiny-za-server.herokuapp.com/collections/hakchurintodo',
-        type: 'POST',
-        dataType: 'json',
-        success: function(response) {
-            submits.response.forEach(function(submit, i, arr) {
+
+var settingsGET = {
+    url: 'http://tiny-za-server.herokuapp.com/collections/hakchurintodo',
+    type: 'GET',
+    success: function(response) {
+
+
+
+        response.forEach(function(item) {
+            // make an li and add it to the ul
+            var deleteButton = $("<button>X</button>");
+            var listItems = $("<li>" + item.todo + "</li>");
+            $('ul').append(listItems);
+            listItems.append(deleteButton);
+            deleteButton.on('click', function() {
+                listItems.remove();
+                console.log(item._id);
+                $.ajax({
+                    type: 'DELETE',
+                    url: 'http://tiny-za-server.herokuapp.com/collections/hakchurintodo/' + item._id,
+                    success: function(response) {
+                        console.log('it deleted');
+                    },
+                    error: function() {
+
+                        console.log('it didnt delete');
+                    }
+
+
+                });
+
 
             });
+        });
+    }
+};
 
-
-            console.log(submits);
-        }
-
-
-
-    });
-
-
-
-
-
-
-
-
-
-//   var container =('.content','.lists','#myForm');
-// response.forEach(function(current) {
-
-
-
-// });
-//
-//   }
-// });
-
-
-
-// $.ajax({
-//
-//
-//     url: 'http://tiny-za-server.herokuapp.com/collections/hakchurintodo',
-//     type: 'GET',
-//     dataType: 'json',
-//     success: function(response) {
-//         var content = $('.content');
-//         response.forEach(function(cuurrent) {
-//
-//             var itemObj = $('lists').addClass('itemObj');
-//             itemObj.append(content.innerText);
-//             //console.log(content);
-//
-//
-//         });
-//     }
-// });
-
-
-
-
-
-
-
-//create li or whatever container
-//append itemObj.listItem
-
-//console.log(itemObj.listItem);
-//     });
-//
-//   }
-// };
-
-
-
-
-
-// $('item').on('click',function(e){
-//   $(e.target).val('complete');
-//   $(this).attr('disabled',true);
-//   setTimeout(function(){
-//    $(e.target).val('complete');
-//   $(e.target).attr('disabled',true);
-//  }, 4000);
-//
-// });
+$.ajax(settingsGET);
